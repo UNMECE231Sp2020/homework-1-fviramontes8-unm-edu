0b688adcb365ec39c287eab6ec88bfc41fd54c49
Andrew Gilbert <andrewigilbert01@gmail.com>
2020-02-08 22:11:31 -0700

    Added two arrays to function pointers.

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "my_complex.h"
 
+
+
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
 
 	return 0;
+	
+	void (*arr_ptr[])(Complex) = {complex_add, complex_sub, complex_mult, complex_div};
+	void (*arr_ptr[])(double, double) = {magnitude, phase};
 }
 //test hahah
