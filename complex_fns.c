fd5a3e3289bb01eb1ad3160fc9015ad242b43229
4mann444 <nickross4444@gmail.com>
2020-02-09 17:58:29 -0700

    added print functionality to all funcions

--- complex_fns.c
+++ complex_fns.c
@@ -3,13 +3,17 @@
 #include "my_complex.h"
 
 double magnitude(Complex c) {
-	return sqrt((c.real*c.real) + (c.imag*c.imag));
+	double ans = sqrt((c.real*c.real) + (c.imag*c.imag));
+	printf("%lf\n", ans);
+	return ans;
 }
 
 double phase(Complex c) {
 	double ph = atan(c.imag/c.real);
 	ph = ph * (180/M_PI);
-	return (ph<0) ? -ph : ph;
+	double ans = (ph<0) ? -ph : ph;
+	printf("%lf\n", ans);
+	return ans;
 }
 
 void print_complex(Complex c) {
@@ -23,21 +27,25 @@ void print_complex(Complex c) {
 
 Complex complex_conj(Complex c) {
 	c.imag *= -1;
+	print_complex(c);
 	return c;
 }
 
 Complex complex_add(Complex c1, Complex c2) {
 	Complex c = {c1.real+c2.real, c1.imag+c2.imag};
+	print_complex(c);
 	return c;
 }
 		
 Complex complex_sub(Complex c1, Complex c2) {
 	Complex c = {c1.real-c2.real, c1.imag-c2.imag};
+	print_complex(c);
 	return c;
 }
 
 Complex complex_mult(Complex c1, Complex c2) {
 	Complex c = {(c1.real*c2.real)+(c1.imag*c2.imag*-1), (c1.imag*c2.real) + (c1.real*c2.imag)};
+	print_complex(c);
 	return c;
 }
 
@@ -50,6 +58,7 @@ Complex complex_div(Complex c1, Complex c2) {
 	Complex c = complex_mult(c1, complex_conj(c2));
 	c.real /= denom;
 	c.imag /= denom;
+	print_complex(c);
 	return c;
 	}
 }
